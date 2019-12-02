a94eac3173d8c4ba7a70c91cb1e520ed47fc60b7
Jerome Bell <jeromebell@gmail.com>
2019-12-02 14:58:55 -0800

    Prevented install of pre-release version of dependencies;

--- setupDevEnvironment.ps1
+++ setupDevEnvironment.ps1
@@ -3,6 +3,6 @@ $origDir = $pwd
 & "$PSScriptRoot\createDevPackages.ps1"
 
 cd $PSScriptRoot\devops\sharecash-dev-vs2019community
-choco install -y sharecash-dev-vs2019community -s "." --pre
+choco install -y sharecash-dev-vs2019community -s "."
 
 cd $origDir
